4063145a561171b835582a98da9dc8c0a00fbe83
IBRAHIM ABDULLAHI <84192666+ioabdullahi@users.noreply.github.com>
2024-02-22 13:29:23 +0100

    Add files via upload

--- lib/presentation/forgot_password_screen/controller/forgot_password_controller.dart
+++ lib/presentation/forgot_password_screen/controller/forgot_password_controller.dart
@@ -1,6 +1,8 @@
 import 'package:empylo/core/app_export.dart';
 import 'package:empylo/presentation/forgot_password_screen/models/forgot_password_model.dart';
 import 'package:flutter/material.dart';
+import 'package:empylo/data/models/forgotPasswordPost/post_forgot_password_post_resp.dart';
+import 'package:empylo/data/apiClient/api_client.dart';
 
 /// A controller class for the ForgotPasswordScreen.
 ///
@@ -11,9 +13,34 @@ class ForgotPasswordController extends GetxController {
 
   Rx<ForgotPasswordModel> forgotPasswordModelObj = ForgotPasswordModel().obs;
 
+  PostForgotPasswordPostResp postForgotPasswordPostResp =
+      PostForgotPasswordPostResp();
+
   @override
   void onClose() {
     super.onClose();
     passwordController.dispose();
   }
+
+  /// Calls the https://empylo-app.vercel.app/auth/user/forgot-password API with the specified request data.
+  ///
+  /// The [Map] parameter represents request body
+  Future<void> callForgotPasswordPost(Map req) async {
+    try {
+      postForgotPasswordPostResp =
+          await Get.find<ApiClient>().forgotPasswordPost(
+        headers: {
+          'Content-type': 'application/json',
+        },
+        requestData: req,
+      );
+      _handleForgotPasswordPostSuccess();
+    } on PostForgotPasswordPostResp catch (e) {
+      postForgotPasswordPostResp = e;
+      rethrow;
+    }
+  }
+
+  /// handles the success response for the API
+  void _handleForgotPasswordPostSuccess() {}
 }
