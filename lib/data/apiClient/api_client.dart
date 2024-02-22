4495d74b2b7ecf639ce8a3b0528d72dc025832e6
IBRAHIM ABDULLAHI <84192666+ioabdullahi@users.noreply.github.com>
2024-02-22 13:33:40 +0100

    4 more endpoints logic integrated

--- lib/data/apiClient/api_client.dart
+++ lib/data/apiClient/api_client.dart
@@ -1,7 +1,10 @@
 import 'package:empylo/core/app_export.dart';
 import 'package:empylo/core/utils/progress_dialog_utils.dart';
+import 'package:empylo/data/models/forgotPasswordPost/post_forgot_password_post_resp.dart';
 import 'package:empylo/data/models/loginUser/post_login_user_resp.dart';
+import 'package:empylo/data/models/resetPassword/post_reset_password_resp.dart';
 import 'package:empylo/data/models/signupUser/post_signup_user_resp.dart';
+import 'package:empylo/data/models/verifyUserAuth/post_verify_user_auth_resp.dart';
 
 class ApiClient extends GetConnect {
   var url = "https://empylo-app.vercel.app";
@@ -25,6 +28,114 @@ class ApiClient extends GetConnect {
   /// user can modify this method with custom logics based on their API response
   bool _isSuccessCall(Response response) {
     return response.isOk;
+  }
+
+  /// Performs API call for https://empylo-app.vercel.app/auth/user/reset-password
+  ///
+  /// Sends a POST request to the server's 'https://empylo-app.vercel.app/auth/user/reset-password' endpoint
+  /// with the provided headers and request data
+  /// Returns a [PostResetPasswordResp] object representing the response.
+  /// Throws an error if the request fails or an exception occurs.
+  Future<PostResetPasswordResp> resetPassword({
+    Map<String, String> headers = const {},
+    Map requestData = const {},
+  }) async {
+    ProgressDialogUtils.showProgressDialog();
+    try {
+      await isNetworkConnected();
+      Response response = await httpClient.post(
+        '$url/auth/user/reset-password',
+        headers: headers,
+        body: requestData,
+      );
+      ProgressDialogUtils.hideProgressDialog();
+      if (_isSuccessCall(response)) {
+        return PostResetPasswordResp.fromJson(response.body);
+      } else {
+        throw response.body != null
+            ? PostResetPasswordResp.fromJson(response.body)
+            : 'Something Went Wrong!';
+      }
+    } catch (error, stackTrace) {
+      ProgressDialogUtils.hideProgressDialog();
+      Logger.log(
+        error,
+        stackTrace: stackTrace,
+      );
+      rethrow;
+    }
+  }
+
+  /// Performs API call for https://empylo-app.vercel.app/auth/user/forgot-password
+  ///
+  /// Sends a POST request to the server's 'https://empylo-app.vercel.app/auth/user/forgot-password' endpoint
+  /// with the provided headers and request data
+  /// Returns a [PostForgotPasswordPostResp] object representing the response.
+  /// Throws an error if the request fails or an exception occurs.
+  Future<PostForgotPasswordPostResp> forgotPasswordPost({
+    Map<String, String> headers = const {},
+    Map requestData = const {},
+  }) async {
+    ProgressDialogUtils.showProgressDialog();
+    try {
+      await isNetworkConnected();
+      Response response = await httpClient.post(
+        '$url/auth/user/forgot-password',
+        headers: headers,
+        body: requestData,
+      );
+      ProgressDialogUtils.hideProgressDialog();
+      if (_isSuccessCall(response)) {
+        return PostForgotPasswordPostResp.fromJson(response.body);
+      } else {
+        throw response.body != null
+            ? PostForgotPasswordPostResp.fromJson(response.body)
+            : 'Something Went Wrong!';
+      }
+    } catch (error, stackTrace) {
+      ProgressDialogUtils.hideProgressDialog();
+      Logger.log(
+        error,
+        stackTrace: stackTrace,
+      );
+      rethrow;
+    }
+  }
+
+  /// Performs API call for https://empylo-app.vercel.app/auth/user/verify
+  ///
+  /// Sends a POST request to the server's 'https://empylo-app.vercel.app/auth/user/verify' endpoint
+  /// with the provided headers and request data
+  /// Returns a [PostVerifyUserAuthResp] object representing the response.
+  /// Throws an error if the request fails or an exception occurs.
+  Future<PostVerifyUserAuthResp> verifyUserAuth({
+    Map<String, String> headers = const {},
+    Map requestData = const {},
+  }) async {
+    ProgressDialogUtils.showProgressDialog();
+    try {
+      await isNetworkConnected();
+      Response response = await httpClient.post(
+        '$url/auth/user/verify',
+        headers: headers,
+        body: requestData,
+      );
+      ProgressDialogUtils.hideProgressDialog();
+      if (_isSuccessCall(response)) {
+        return PostVerifyUserAuthResp.fromJson(response.body);
+      } else {
+        throw response.body != null
+            ? PostVerifyUserAuthResp.fromJson(response.body)
+            : 'Something Went Wrong!';
+      }
+    } catch (error, stackTrace) {
+      ProgressDialogUtils.hideProgressDialog();
+      Logger.log(
+        error,
+        stackTrace: stackTrace,
+      );
+      rethrow;
+    }
   }
 
   /// Performs API call for https://empylo-app.vercel.app/auth/user/login
